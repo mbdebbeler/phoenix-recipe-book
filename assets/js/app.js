5d9f31aa13bc05e8e4529dd22a6c5cd2f4393fde
Monica Debbeler <m.b.debbeler@gmail.com>
2020-02-03 23:56:41 -0500

    Extract ReactDom to main.js, make main.js entry point

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,16 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
-import css from "../css/app.css"
-
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
-import "phoenix_html"
 import React from "react"
-import ReactDOM from "react-dom"
+import "phoenix_html"
 import Recipes from "./containers/Recipes"
+import Networking from "./networking"
 
 class App extends React.Component {
   render() {
     return (
-      <Recipes />
+      <div>
+      <Recipes networking={new Networking()} />
+      </div>
     )
   }
 }
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById("app")
-)
+export default App
