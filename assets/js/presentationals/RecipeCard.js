5a96335a20a09df1711f6f92a7c1d184de877147
Monica Debbeler <m.b.debbeler@gmail.com>
2020-02-07 14:21:54 -0500

    Align color theme with mockups

--- assets/js/presentationals/RecipeCard.js
+++ assets/js/presentationals/RecipeCard.js
@@ -16,23 +16,23 @@ class RecipeCard extends React.Component {
 
   render () {
     return (
-    <div className="mdc-layout-grid__cell">
-      <div className="mdc-card my-card-content">
-        <button className={"mdc-icon-button material-icons mdc-card__action mdc-card__action--icon--unbounded my-more-vert mdc-menu-surface--anchor"} onClick={this.handleClick} title="More options" data-mdc-ripple-is-unbounded="true">more_vert <CardMenu id={this.props.id} isToggleOn={this.state.isToggleOn}/> </button>
-        <div className="mdc-card__primary-action demo-card__primary-action" tabIndex="0">
-          <div className="demo-card__primary">
-            <h2 className="demo-card__title mdc-typography mdc-typography--headline6">{this.props.title}</h2>
-            <h3 className="demo-card__subtitle mdc-typography mdc-typography--subtitle2">Serves: {this.props.min_servings} to {this.props.max_servings}</h3>
+      <div className="mdc-layout-grid__cell">
+        <div className="mdc-card my-card-content">
+          <button className={"mdc-icon-button material-icons mdc-card__action mdc-card__action--icon--unbounded my-more-vert mdc-menu-surface--anchor"} onClick={this.handleClick} title="More options" data-mdc-ripple-is-unbounded="true">more_vert <CardMenu id={this.props.id} isToggleOn={this.state.isToggleOn}/> </button>
+          <div className="mdc-card__primary-action demo-card__primary-action" tabIndex="0">
+            <div className="demo-card__primary">
+              <h2 className="demo-card__title mdc-typography mdc-typography--headline6">{this.props.title}</h2>
+              <h3 className="demo-card__subtitle mdc-typography mdc-typography--subtitle2">Serves: {this.props.min_servings} to {this.props.max_servings}</h3>
+            </div>
+          </div>
+          <div className="mdc-card__actions">
+            <div className="mdc-card__action-buttons">
+              <button className="mdc-button mdc-button--raised">  <span className="mdc-button__ripple"></span> View</button>
+            </div>
           </div>
         </div>
-        <div className="mdc-card__actions">
-          <div className="mdc-card__action-buttons">
-            <button className="mdc-button mdc-card__action mdc-card__action--button">  <span className="mdc-button__ripple"></span> View</button>
-          </div>
-        </div>
-      </div>
     </div>
-    )
-  }
+  )
+}
 }
 export default RecipeCard
